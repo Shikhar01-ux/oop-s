5e2ba18c75228311c9994edb0db7df69ce3b0205
Shikhar01-ux <yadavshikhar613@gmail.com>
2026-01-16 19:13:20 +0530

    define how to give different values by constructor

---
+++ value.py
@@ -0,0 +1,10 @@
+class student():
+    def __init__(self, name, year):
+        self.name = name 
+        self.year = year
+
+stu1 = student("Shikhar yadav",1)
+stu2 = student("Sovind Yadav",1)
+
+print(stu1.name)
+print(stu2.year)
